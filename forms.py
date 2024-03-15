8545b8a1369cb46c4c3eebece199ab4060a4a378
mfarrukh1 <smfarrukh@live.com>
2024-03-15 07:39:21 +0500

    Translation and Login

--- forms.py
+++ forms.py
@@ -11,23 +11,23 @@ def ValidateDirectory(form, field):
             raise ValidationError("无效的目录！")
 
 class SettingsForm(FlaskForm):
-    GStationName = StringField("Ground Station Name", validators=[Length(max=20)], render_kw={"placeholder": "Earth Station"})
-    latitude = DecimalField("Latitude", places=6, description='Latitude', render_kw={"placeholder": "Latitude"}, validators=[optional()])
-    longitude = DecimalField("Longitude", places=6, description='Longitude', render_kw={"placeholder": "Longitude"}, validators=[optional()])
-    altitude = IntegerField("Altitude", description="From Mean Sea Level", render_kw={"placeholder": "From Mean Sea Level"}, validators=[optional()])
-    minElevation = IntegerField("Minimum Elevation", description="Minimum Elevation Pass to Recieve", render_kw={"placeholder": "Minimum Elevation to Receive"}, validators=[optional()])
-    Timezone = SelectField("Timezone", description="Default Timezone", choices=[(datetime.datetime.now().astimezone().tzinfo, 'Local Time'),
-                                                                                (datetime.timezone.utc,'UTC')],
-                                                                                render_kw={"placeholder": "Default Timezone"})
-    TLESource = SelectField("TLE Source", description="Default TLE Source", 
-                            choices=[("https://celestrak.org/NORAD/elements/gp.php?CATNR={:d}","celestrak"),
-                                     ("https://db.satnogs.org/api/tle/?format=3le&norad_cat_id={:d}","satnogs") ],
-                                     render_kw={"placeholder": "Default TLE Source"})
+    # GStationName = StringField("Ground Station Name", validators=[Length(max=20)], render_kw={"placeholder": "Earth Station"})
+    # latitude = DecimalField("Latitude", places=6, description='Latitude', render_kw={"placeholder": "Latitude"}, validators=[optional()])
+    # longitude = DecimalField("Longitude", places=6, description='Longitude', render_kw={"placeholder": "Longitude"}, validators=[optional()])
+    # altitude = IntegerField("Altitude", description="From Mean Sea Level", render_kw={"placeholder": "From Mean Sea Level"}, validators=[optional()])
+    # minElevation = IntegerField("Minimum Elevation", description="Minimum Elevation Pass to Recieve", render_kw={"placeholder": "Minimum Elevation to Receive"}, validators=[optional()])
+    # Timezone = SelectField("Timezone", description="Default Timezone", choices=[(datetime.datetime.now().astimezone().tzinfo, 'Local Time'),
+    #                                                                             (datetime.timezone.utc,'UTC')],
+    #                                                                             render_kw={"placeholder": "Default Timezone"})
+    # TLESource = SelectField("TLE Source", description="Default TLE Source", 
+    #                         choices=[("https://celestrak.org/NORAD/elements/gp.php?CATNR={:d}","celestrak"),
+    #                                  ("https://db.satnogs.org/api/tle/?format=3le&norad_cat_id={:d}","satnogs") ],
+    #                                  render_kw={"placeholder": "Default TLE Source"})
     
-    AudioDirectory = StringField("Wav File Directory", description="D:/NOAA-wav", validators=[ValidateDirectory],
-                                 render_kw={"placeholder": r"D:\NOAA-wav"})
-    ImageDirectory = StringField("Product Directory", description="C:/Users/DELL/Documents/NOAA-Images", validators=[ValidateDirectory],
-                                 render_kw={"placeholder": r"C:\Users\DELL\Documents\NOAA-Images"})
+    # AudioDirectory = StringField("Wav File Directory", description="D:/NOAA-wav", validators=[ValidateDirectory],
+    #                              render_kw={"placeholder": r"D:\NOAA-wav"})
+    # ImageDirectory = StringField("Product Directory", description="C:/Users/DELL/Documents/NOAA-Images", validators=[ValidateDirectory],
+    #                              render_kw={"placeholder": r"C:\Users\DELL\Documents\NOAA-Images"})
     ###
 
     GStationName = StringField("地面站名称", validators=[Length(max=20)], render_kw={"placeholder": "地球站"})
@@ -50,25 +50,44 @@ class SettingsForm(FlaskForm):
 
         
 class RegistrationForm(FlaskForm):
-    email = StringField('Email',
+    # email = StringField('Email',
+    #                     validators=[DataRequired(), Email()],
+    #                     render_kw={"placeholder":"example@company.com"})
+    # password = PasswordField('Password', validators=[DataRequired()])
+    # confirm_password = PasswordField('Confirm Password',
+    #                                  validators=[DataRequired(), EqualTo('password')])
+    # TnC = BooleanField('Terms and Conditions')
+    # submit = SubmitField('Sign Up')
+    # def validate_email(self, email):
+    #     user = User.query.filter_by(email=email.data).first()
+    #     if user:
+    #         raise ValidationError('That email is taken. Please choose a different one.')
+
+    email = StringField('电子邮件',
                         validators=[DataRequired(), Email()],
                         render_kw={"placeholder":"example@company.com"})
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',
+    password = PasswordField('密码', validators=[DataRequired()])
+    confirm_password = PasswordField('确认密码',
                                      validators=[DataRequired(), EqualTo('password')])
-    TnC = BooleanField('Terms and Conditions')
-    submit = SubmitField('Sign Up')
+    TnC = BooleanField('条款和条件')
+    submit = SubmitField('报名')
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
-            raise ValidationError('That email is taken. Please choose a different one.')
+            raise ValidationError('该电子邮件已被占用。请选择一个不同的。')
         
 
 class LoginForm(FlaskForm):
-    email = StringField('Email',
+    # email = StringField('Email',
+    #                     validators=[DataRequired(), Email()],
+    #                     render_kw={"placeholder":"example@company.com"})
+    # password = PasswordField('Password', validators=[DataRequired()])
+    # remember = BooleanField('Remember Me')
+    # submit = SubmitField('Login')
+
+    email = StringField('电子邮件',
                         validators=[DataRequired(), Email()],
                         render_kw={"placeholder":"example@company.com"})
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
-    submit = SubmitField('Login')
-
+    password = PasswordField('密码', validators=[DataRequired()])
+    remember = BooleanField('记住账号')
+    submit = SubmitField('登录')
